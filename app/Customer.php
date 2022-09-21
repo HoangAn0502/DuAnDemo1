aca30b28afc31acf8fe221356d3e0bae36df965f
HoangAn0502 <113957929+HoangAn0502@users.noreply.github.com>
2022-09-21 11:36:17 +0700

    Uđate du an larave

---
+++ app/Customer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    public $timestamp = false; //k set thoi gian
+    protected $fillable = [
+        '','','','','','',''
+    ];
+}
